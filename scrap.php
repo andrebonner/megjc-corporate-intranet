ac32e143ff6535ed59447124f29ec2f37abc4ece
Tremaine Buchanan <tremainekbuchanan@gmail.com>
2016-04-06 11:40:26 -0500

    Added slim

---
+++ scrap.php
@@ -0,0 +1,11 @@
+<?php
+  $content = file_get_contents("http://jamaica-gleaner.com/feed/rss.xml");
+  $x = new SimpleXmlElement($content);
+
+ echo "<ul>";
+
+ foreach($x->channel->item as $entry) {
+     echo "<li><a href='$entry->link' title='$entry->title'>" . $entry->title . " " . $entry->pubDate . "</a></li>";
+ }
+ echo "</ul>";
+?>
