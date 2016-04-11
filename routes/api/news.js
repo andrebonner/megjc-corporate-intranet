8bf7598159364c8d3f49ac5dce28f111808794bd
Tremaine Buchanan <tremainekbuchanan@gmail.com>
2016-04-11 16:02:15 -0500

    Finished rss news filter

--- routes/api/news.js
+++ routes/api/news.js
@@ -3,7 +3,25 @@
 	var express = require('express'),
 		router = express.Router(),
 		request = require('request'),
-		_ = require('underscore');
+		_ = require('underscore'),
+		now = require('performance-now'),
+		keywords = [
+			'agro investment corporation', 'development bank of jamaica', 'dbj', 'factories corporation of jamaica',
+			'harmonization limited', 'imf cordination unit', 'international financial service authority', 
+			'jamaica trade and invest', 'jamaica promotions corporation', 'jampro', 'planning institute of jamaica', 'pioj', 'jamaica business development centre',
+			'jbdc', 'self start fund', 'micro investment development agency', 'mida', 'kingston container terminal', 'kct',
+			'kingston free zone', 'montego bay free zone', 'national housing trust', 'nht', 'port authority of jamaica', 'paj',
+			'port authority management services', 'statistical institute of jamaica', 'statin', 'urban development corporation',
+			'land administration and management programme', 'lamp', 'land access for national development project', 'l.a.n.d',
+			'land divestment advisory committee', 'nla', 'national land agency', 'national spatial data management division',
+			'beach control authority', 'forestry department', 'meteorological services division', 'national environment and planning agency',
+			'nepa', 'national resource and conservation authority', 'nrca', 'town planning department', 'town & country planning authority',
+			'negril green island area local planning authority', 'nigalpa', 'commission of strata corporations', 'jamaica mortgage bank',
+			'housing agency of jamaica', 'haj', 'real estate board', 'relocation of human settlements', 'rent assessment board', 'nwc',
+			'national water commission', 'rural water supply limited', 'rwsl', 'water resources authority', 'wra', 'central wastewater treatment company',
+			'cwtc', 'national road operating and contruction company', 'nrooc', 'national works agency', 'road maintenance fund', 'rmf', 'toll authority',
+			'toll regulator', 'dr horace chang', 'horace chang', 'water', 'housing', 'environment', 'climate change', 'climate', 'investment'
+		];
 	/**
 	 * [description]
 	 * @param  {[type]} req      [description]
@@ -19,13 +37,14 @@
 	 	request(abs_url, function(err, res, body){
 	 		'use strict';
 	 		var data = JSON.parse(body);
-
+	 		
 	 		if(!err && res.statusCode  === 200 && data.status === 'ok'){	 		
 	 			var news_items  = [];
 	 			if(req.query.limit === 'three'){
-	 				news_items = data.items.slice(0,3);
+	 				// news_items = data.items.slice(0,3);
+	 				news_items = filterFeed(data.items, keywords);
 	 			}else if(req.query.limit === 'all'){
-	 				news_items = data;
+	 				news_items = data.items
 	 			}
 	 			response.json(news_items);	 			
 	 		}else{
@@ -33,6 +52,46 @@
 	 		}
 	 	});
 	});
+
+	/**
+	 * 
+	 * @param  {[type]} items    [description]
+	 * @param  {[type]} keywords [description]
+	 * @return {[type]}          [description]
+	 */
+	function filterFeed(items, keywords){
+		var i = keywords.length,
+			k = items.length;
+			result = [],
+			start = now();
+		while(i--){			
+			k = items.length;
+			while(k--){
+				if(keywordFilter(keywords[i], items[k].title.toLowerCase())){
+					console.log(keywords[i])
+					result.push(items[k]);
+				}
+			}
+		}
+		var end = now();
+		console.log((end-start).toFixed(3));
+		return result;
+	}
+
+	 /**
+	 * 
+	 * @param  {[type]} keyword [description]
+	 * @param  {[type]} title   [description]
+	 * @return {[type]}         [description]
+	 */
+	function keywordFilter(keyword, title){
+		var found = false;
+		if(title.search(keyword) >= 0){
+			found = true;
+
+		}
+		return found;
+	}
 
 	module.exports = router;
 
