0d4c90aa26a3c18a38c8ae485db0ca03e2a3aaac
Tremaine Buchanan <tremainekbuchanan@gmail.com>
2016-05-03 20:09:57 -0500

    Added photos for directory page

--- public/app/modules/directory/directory.controller.js
+++ public/app/modules/directory/directory.controller.js
@@ -9,10 +9,15 @@
 		// $http.get('/api/employees').then(function(employees){
 		// 	$scope.employees = employees.data;
 		// });
+		$scope.departments = getDepartments();
 		$scope.employees = [{"name": "Patrick Thompson",
 					"position": "Director",
 					"email": "patrick.thompson@mwlecc.gov.jm",
 					"extension": "2918",
 					"cug": "876-564-0131"}];
+
+		function getDepartments(){
+			return [{"name": "Public Relations"},{"name": "ICT Division"},{"name":"Executive Management"},]
+		}
 	}
 })();
