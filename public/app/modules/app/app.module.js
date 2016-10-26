dc15ba614e0fd4def8c2c0903d62e5c14dc51e82
Tremaine Buchanan <tremainekbuchanan@gmail.com>
2016-10-26 13:38:30 -0500

    disabling cache

--- public/app/modules/app/app.module.js
+++ public/app/modules/app/app.module.js
@@ -27,8 +27,15 @@
 				base_url : '/intranet/api/v1/'
 		});
 
-	function config($routeProvider){
-		$routeProvider.otherwise({redirectTo: '/'});
+	function config($routeProvider, $httpProvider){
+		if (!$httpProvider.defaults.headers.get) {
+			 $httpProvider.defaults.headers.get = {};
+	 }
+	 $httpProvider.defaults.headers.get['If-Modified-Since'] = 'Mon, 26 Jul 1997 05:00:00 GMT';
+	 $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
+	 $httpProvider.defaults.headers.get['Pragma'] = 'no-cache';
+
+	 $routeProvider.otherwise({redirectTo: '/'});
 	}
 	/**
 	 * Checks if an application route is protected.
